c42a34885eae2a88c24e96e1312fb65aa929d309
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-08-16 12:27:52 -0500

    counter: only allow owner to close counter

--- playground/src/commands/counter.ts
+++ playground/src/commands/counter.ts
@@ -3,42 +3,33 @@ import {
   buttonComponent,
   defineSlashCommand,
 } from "@itsmapleleaf/gatekeeper"
-import { wait } from "../wait"
 
 export const counterCommand = defineSlashCommand({
   name: "counter",
   description: "make a counter",
   async run(context) {
     let count = 0
-    let state = "running"
 
-    const reply = await context.createReply(() => {
-      if (state === "done") {
-        return ["well fine then"]
-      }
-
-      return [
-        `button pressed ${count} times`,
-        actionRowComponent(
-          buttonComponent({
-            style: "PRIMARY",
-            label: "press it",
-            onClick: () => {
-              count += 1
-            },
-          }),
-          buttonComponent({
-            style: "PRIMARY",
-            label: "i'm bored",
-            onClick: async () => {
-              state = "done"
-              await reply.update()
-              await wait(1000)
+    const reply = await context.createReply(() => [
+      `button pressed ${count} times`,
+      actionRowComponent(
+        buttonComponent({
+          style: "PRIMARY",
+          label: "press it",
+          onClick: () => {
+            count += 1
+          },
+        }),
+        buttonComponent({
+          style: "PRIMARY",
+          label: "done",
+          onClick: async (event) => {
+            if (event.user.id === context.user.id) {
               await reply.delete()
-            },
-          }),
-        ),
-      ]
-    })
+            }
+          },
+        }),
+      ),
+    ])
   },
 })
