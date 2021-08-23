d9ad1fbbdbd47fe8d316b0c75f3957bf05e0194e
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-08-23 00:50:30 -0500

    add an embed example

---
+++ packages/playground/src/commands/embeds.ts
@@ -0,0 +1,42 @@
+import {
+  defineUserCommand,
+  embedComponent,
+} from "@itsmapleleaf/gatekeeper/src/main"
+import type { EmbedFieldData } from "discord.js"
+
+export const userInfoCommand = defineUserCommand({
+  name: "Get User Info",
+  run(context) {
+    const fields: EmbedFieldData[] = []
+
+    if (context.targetGuildMember) {
+      fields.push({
+        name: "Color",
+        value: context.targetGuildMember.displayHexColor,
+      })
+
+      const roles = context.targetGuildMember.roles.cache.filter(
+        (role) => role.name !== "@everyone",
+      )
+
+      if (roles.size > 0) {
+        fields.push({
+          name: "Roles",
+          value: roles.map((role) => `<@&${role.id}>`).join(" "),
+        })
+      }
+    }
+
+    context.reply(() =>
+      embedComponent({
+        title:
+          context.targetGuildMember?.displayName ?? context.targetUser.username,
+        color: context.targetGuildMember?.displayColor,
+        thumbnail: {
+          url: context.targetUser.avatarURL() ?? undefined,
+        },
+        fields,
+      }),
+    )
+  },
+})
