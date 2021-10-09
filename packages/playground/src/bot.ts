2bb5e5bec21573758ea03a8abc21bd1f8c9d1774
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-10-09 00:37:01 -0500

    playground: use new stuff

--- packages/playground/src/bot.ts
+++ packages/playground/src/bot.ts
@@ -1,97 +1,19 @@
-import { buttonComponent } from "@itsmapleleaf/gatekeeper/src/core.new/button-component"
-import { createGatekeeper } from "@itsmapleleaf/gatekeeper/src/core.new/gatekeeper"
-import type { InteractionContext } from "@itsmapleleaf/gatekeeper/src/core.new/interaction-context"
-import { defineUserCommand } from "@itsmapleleaf/gatekeeper/src/core.new/user-command"
+import { createGatekeeper } from "@itsmapleleaf/gatekeeper/src/main"
 import { Client, Intents } from "discord.js"
 import "dotenv/config"
-import { setTimeout } from "node:timers/promises"
+import { join } from "path/posix"
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS],
 })
 
-createGatekeeper({
-  client,
-  commands: [
-    defineUserCommand({
-      name: "hug",
-      async run(context) {
-        const reply = context.reply(() => "the")
-        await setTimeout(1000)
-        reply.delete()
-      },
-    }),
+void (async () => {
+  await createGatekeeper({
+    name: "playground-bot",
+    client,
+    commandFolder: join(__dirname, "commands"),
+  })
 
-    defineUserCommand({
-      name: "countdown",
-      async run(context) {
-        let count = 5
-        const reply = context.reply(() => count)
-
-        do {
-          await setTimeout(1000)
-          count -= 1
-          reply.refresh()
-        } while (count > 0)
-
-        await setTimeout(1000)
-        reply.delete()
-      },
-    }),
-
-    defineUserCommand({
-      name: "counter",
-      async run(context) {
-        let count = 0
-        context.ephemeralReply(() => [
-          `count: ${count}`,
-          buttonComponent({
-            label: `add 1 (${count})`,
-            style: "PRIMARY",
-            onClick: () => {
-              count += 1
-            },
-          }),
-        ])
-      },
-    }),
-
-    defineUserCommand({
-      name: "message factory",
-      async run(context) {
-        function createReply(context: InteractionContext) {
-          const reply = context.reply(() => [
-            buttonComponent({
-              label: "create message",
-              style: "PRIMARY",
-              onClick: (context) => {
-                createReply(context)
-              },
-            }),
-            buttonComponent({
-              label: "delete",
-              style: "DANGER",
-              onClick: () => {
-                reply.delete()
-              },
-            }),
-          ])
-        }
-
-        createReply(context)
-      },
-    }),
-
-    // defineUserCommand({
-    //   name: "defertest",
-    //   async run(context) {
-    //     context.ephemeralDefer()
-    //     context.reply(() => "this should be public")
-    //     context.ephemeralReply(() => "this should be private")
-    //   },
-    // }),
-  ],
-})
-
-// eslint-disable-next-line no-console
-client.login(process.env.BOT_TOKEN).catch(console.error)
+  // eslint-disable-next-line no-console
+  await client.login(process.env.BOT_TOKEN)
+})()
