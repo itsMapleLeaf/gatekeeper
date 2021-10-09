6b861c377ce0111b22ee5b09d5fa03d68eb53042
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-10-09 00:57:44 -0500

    simplify rollup config

--- packages/gatekeeper/rollup.config.js
+++ packages/gatekeeper/rollup.config.js
@@ -1,5 +1,3 @@
-import commonjs from "@rollup/plugin-commonjs"
-import nodeResolve from "@rollup/plugin-node-resolve"
 import { createRequire } from "module"
 import typescript from "rollup-plugin-ts"
 
@@ -9,25 +7,21 @@ const pkg = require("./package.json")
 
 export default {
   input: "./src/main.ts",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      sourcemap: true,
-    },
-  ],
+  output: {
+    file: pkg.main,
+    format: "cjs",
+    sourcemap: true,
+  },
   plugins: [
-    typescript({ tsconfig: "../../tsconfig.json" }),
-    nodeResolve({ extensions: [".js", ".ts"] }),
-    commonjs(),
+    typescript({
+      tsconfig: "../../tsconfig.json",
+      transpileOnly: true,
+    }),
   ],
   external: [
     ...Object.keys(pkg.peerDependencies),
     ...Object.keys(pkg.dependencies),
+    "node:path",
+    "node:crypto",
   ],
 }
