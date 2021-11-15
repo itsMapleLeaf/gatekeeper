7e234355fa8b6b955bc523ad7dbf0e2a647685f4
Crawron <azertyaaron@gmail.com>
2021-11-15 15:21:26 -0500

    Allow for channel type for the CHANNEL option type

---
+++ playground/src/commands/channel-types.ts
@@ -0,0 +1,42 @@
+import type { Gatekeeper } from "@itsmapleleaf/gatekeeper/src/main"
+import { createSecretKey } from "crypto"
+
+export default function defineCommands(gatekeeper: Gatekeeper) {
+  gatekeeper.addSlashCommand({
+    name: "channel-types",
+    description: "Test channel types",
+    options: {
+      text: {
+        type: "CHANNEL",
+        description: "A text channel",
+        channelTypes: ["GUILD_TEXT"],
+      },
+      voice: {
+        type: "CHANNEL",
+        description: "A voice channel",
+        channelTypes: ["GUILD_VOICE"],
+      },
+      category: {
+        type: "CHANNEL",
+        description: "A category",
+        channelTypes: ["GUILD_CATEGORY"],
+      },
+      any: {
+        type: "CHANNEL",
+        description: "Any channel",
+      },
+    },
+    run(context) {
+      const { any, category, text, voice } = context.options
+
+      const selection = [
+        any?.name,
+        category?.name,
+        text?.name,
+        voice?.name,
+      ].filter((s) => !!s)
+
+      context.reply(() => `You selected ${selection}`)
+    },
+  })
+}
