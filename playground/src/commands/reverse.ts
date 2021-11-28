ebfe8ef4a67493fb6988decdb15d1531faff5435
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-11-27 20:33:39 -0600

    add aliases and fixes for some playground commands

--- playground/src/commands/reverse.ts
+++ playground/src/commands/reverse.ts
@@ -3,9 +3,13 @@ import type { Gatekeeper } from "@itsmapleleaf/gatekeeper"
 export default function defineCommands(gatekeeper: Gatekeeper) {
   gatekeeper.addMessageCommand({
     name: "reverse message content",
+    aliases: ["rev"],
     run(context) {
       context.reply(() =>
-        context.targetMessage.content.split("").reverse().join(""),
+        (context.targetMessage.content || "no message content")
+          .split("")
+          .reverse()
+          .join(""),
       )
     },
   })
